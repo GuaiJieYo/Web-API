4757c1a4781d77f36f92972fdf5fe7dde11cd22a
GuaiJieYo <guaijieaz@gmail.com>
2022-03-13 13:12:46 +0800

    wdadjkaw

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 $file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 if(file_exists($file)){
-    if (strstr($_SERVER['SCRIPT_NAME'],"/api/")) {
+    if ($_SERVER['SCRIPT_NAME'] == "/api/index.php") {
         require_once __DIR__ . '/../404.html';
     }else {
         return false;
