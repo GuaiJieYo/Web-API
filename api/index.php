2debad822e19aa0f1d4fcb730700ab4930437b5d
GuaiJieYo <guaijieaz@gmail.com>
2022-03-13 13:06:27 +0800

    2022年3月13日

--- api/index.php
+++ api/index.php
@@ -2,7 +2,11 @@
 error_reporting(0);
 $file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 if(file_exists($file)){
-   return false;
+    if (strstr(__FILE__,"/api/")) {
+        require_once __DIR__ . '/../404.html';
+    }else {
+        return false;
+    }
 }else {
     require_once __DIR__ . '/../404.html';
 }
