4b4f5d08ef4fd50589bb4e8ffd5f461ed0290ef2
GuaiJieYo <guaijieaz@gmail.com>
2023-06-17 13:24:30 +0800

    123

--- api/bing/index.js
+++ api/bing/index.js
@@ -1,17 +1,16 @@
 const fetch = require('node-fetch');
-
-const BING_API_URL = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
+const url = require('url');
 
 module.exports = async (req, res) => {
   try {
-    const uhd = req.query.uhd === 'true';
-    const response = await fetch(BING_API_URL);
-    const { images } = await response.json();
-    let imageUrl = `https://cn.bing.com${images[0].url}`;
-    
-    return res.redirect(302, imageUrl);
+    const bingApiUrl = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
+    const response = await fetch(bingApiUrl);
+    const data = await response.json();
+    const imageUrl = `https://cn.bing.com${data.images[0].url}`;
+    const urlObj = url.parse(imageUrl);
+    return res.redirect(302, url.format(urlObj));
   } catch (err) {
     console.error(err);
-    return res.status(500).send('网络错误');
+    return res.status(500).send('Internal Server Error');
   }
 };
