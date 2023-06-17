d7a959a92e85c1d1d98ccd277c4799b3647d2a56
GuaiJieYo <guaijieaz@gmail.com>
2023-06-17 13:29:27 +0800

    1

--- api/bing/index.js
+++ api/bing/index.js
@@ -1,16 +1,33 @@
-const fetch = require('node-fetch');
-const url = require('url');
+const https = require('https');
+const { URL } = require('url');
 
-module.exports = async (req, res) => {
-  try {
-    const bingApiUrl = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
-    const response = await fetch(bingApiUrl);
-    const data = await response.json();
-    const imageUrl = `https://cn.bing.com${data.images[0].url}`;
-    const urlObj = url.parse(imageUrl);
-    return res.redirect(302, url.format(urlObj));
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send('Internal Server Error');
+module.exports = (req, res) => {
+  // 构建请求 URL
+  const apiUrl = new URL('https://bing.com/HPImageArchive.aspx');
+  apiUrl.searchParams.append('format', 'js');
+  apiUrl.searchParams.append('idx', '0');
+  apiUrl.searchParams.append('n', '1');
+  if (req.query.uhd === 'true') {
+    apiUrl.searchParams.append('uhd', '1');
   }
+
+  // 发送 HTTP 请求
+  https.get(apiUrl, (apiRes) => {
+    let body = '';
+    apiRes.on('data', (chunk) => {
+      body += chunk;
+    });
+    apiRes.on('end', () => {
+      // 解析 JSON 响应
+      const data = JSON.parse(body);
+      const imageUrl = `https://bing.com${data.images[0].url}`;
+
+      // 发送重定向响应
+      res.setHeader('Location', imageUrl);
+      res.status(302).send();
+    });
+  }).on('error', (err) => {
+    console.error(`请求失败：${err.message}`);
+    res.status(500).send('请求失败');
+  });
 };
